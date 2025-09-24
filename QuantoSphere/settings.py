c621926cff36347144acaf0dc5c9c6a742fa3f23
pablopedrosap <113117881+pablopedrosap@users.noreply.github.com>
2025-09-24 12:34:39 -0500

    Update settings.py

--- QuantoSphere/settings.py
+++ QuantoSphere/settings.py
@@ -1,4 +1,5 @@
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z8cogn%1@3c&oj@mrn31m1&z4tj&^yxhb5g+dj&@v4_h@vo)@h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pablopedrosap@gmail.com'
-EMAIL_HOST_PASSWORD = 'kingpablo'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 REST_FRAMEWORK = {
